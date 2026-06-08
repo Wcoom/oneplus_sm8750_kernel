187512fccd612a98114cdcd7714838439c7996bf
whitewhale0612 <2136512929@qq.com>
2026-06-08 14:51:43 +0800

    sched: fix a problem

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -180,10 +180,9 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 				  unsigned long util, unsigned long max)
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
-	unsigned int freq;
+	unsigned int freq = arch_scale_freq_invariant() ?
+				policy->cpuinfo.max_freq : policy->cur;
 	unsigned long next_freq = 0;
-
-	freq = get_capacity_ref_freq(policy);
 	trace_android_vh_map_util_freq(util, freq, max, &next_freq, policy,
 			&sg_policy->need_freq_update);
 	if (next_freq)
